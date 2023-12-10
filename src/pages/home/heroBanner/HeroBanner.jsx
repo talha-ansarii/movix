abdc2e01e1155188720a3d6b6f763624ca7ded1c
talha-ansarii <140020341+talha-ansarii@users.noreply.github.com>
2023-12-11 01:25:38 +0530

    Update HeroBanner.jsx

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -44,7 +44,7 @@ const HeroBanner = () => {
         <ContentWrapper>
         <div className='heroBannerContent'>
             <span className='title'>Welcome.</span>
-            <span className='subTitle'>Millions of movies, TV shows and people to discover. Explore now.</span>
+            <span className='subTitle'>Millions of movies and TV shows to discover. Explore now.</span>
             <div className='searchInput'>
                 <input 
                     type='text'
